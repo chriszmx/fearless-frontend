602a3a7bf013dc0c23c28dc15e3b6d1d9f879fe5
Chris <c.r.zambito@gmail.com>
2023-04-17 20:09:08 -0400

    added conference form

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,5 +1,8 @@
 import { Fragment } from "react";
 import Nav from "./Nav";
+import AttendeesList from "./AttendeesList";
+import LocationForm from "./LocationForm";
+import ConferenceForm from "./ConferenceForm";
 
 function App(props) {
   if (props.attendees === undefined) {
@@ -9,30 +12,9 @@ function App(props) {
     <Fragment>
     <Nav />
     <div className="container">
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Conference</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* for (let attendee of props.attendees) {
-            <tr key={attendee.href}>
-              <td>{ attendee.name }</td>
-              <td>{ attendee.conference }</td>
-            </tr>
-          } */}
-          {props.attendees.map(attendee => {
-            return (
-              <tr key={attendee.href}>
-                <td>{ attendee.name }</td>
-                <td>{ attendee.conference }</td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+      <ConferenceForm />
+      {/* <LocationForm /> */}
+      {/* <AttendeesList attendees={props.attendees} /> */}
     </div>
     </Fragment>
   );
